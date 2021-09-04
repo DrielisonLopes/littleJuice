c0e429d5cf25f0e9fd41e71de1417c70138b5b2b
unknown <rsfbernardes@hotmail.com>
2021-09-04 15:30:25 -0300

    configuracao do eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,9 +4,13 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'prettier'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -15,10 +19,26 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
+  settings: {
+    'settings': {
+      'import/resolver': {
+        'node': {
+          'extensions': ['.js', '.ts']
+        }
+      }
+    }
+  },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/extensions': ['error', 'ignorePackages', {'js': never, 'ts': never}],
+    'import/prefer-default-export': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-constructor': 'off',
+    'no-empty-function': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn'
   },
 };
