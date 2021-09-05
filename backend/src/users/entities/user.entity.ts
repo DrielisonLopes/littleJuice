d211b473aa59b317ce23312cc877a1049542497d
unknown <rsfbernardes@hotmail.com>
2021-09-05 17:09:43 -0300

    config da entidade users

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,6 +1,29 @@
-import { Table, Model } from 'sequelize-typescript';
+import { All } from '@nestjs/common';
+import { col } from 'sequelize';
+import { Model } from 'sequelize';
+import { AllowNull, AutoIncrement, Column, PrimaryKey, Table, Unique } from 'sequelize-typescript';
 
 @Table
 export class Users extends Model {
-    
+    @AutoIncrement
+    @PrimaryKey
+    @Column
+    id: number;
+
+    @AllowNull(false)
+    @Column
+    name: string;
+
+    @AllowNull(false)
+    @Unique
+    @Column
+    email: string;
+
+    @AllowNull(false)
+    @Column
+    password: string;
+
+    @AllowNull(false)
+    @Column
+    location: string;
 }
