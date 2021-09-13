f7a09953f7d917c8a38c2eab8a0ebbddf646941f
unknown <rsfbernardes@hotmail.com>
2021-09-12 22:23:51 -0300

    validação em todos os dto's

--- backend/src/schedule/dto/update-schedule.dto.ts
+++ backend/src/schedule/dto/update-schedule.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, IsNotEmpty, IsDate} from "class-validator";
+
 export class UpdateScheduleDto {
+
+    @IsString()
+    @IsNotEmpty()
     location_schedule: string;
 
+    @IsString()
+    @IsNotEmpty()
+    @IsDate()
     date: Date;
 }
