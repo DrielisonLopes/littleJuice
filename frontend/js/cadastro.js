fbc9bc5aacde2f489744122ffbebcdfad094a3c3
agnaldo26 <agnaldosilva9704@gmail.com>
2021-09-15 15:49:35 -0300

    corrigido o cadastro do usuário

--- frontend/js/cadastro.js
+++ frontend/js/cadastro.js
@@ -1,45 +1,66 @@
-const inputSenha = document.getElementById("senha");
-const inputConfirmarSenha = document.getElementById("confirmar-senha");
+const senha = document.getElementById("senha");
+const confirmarSenha = document.getElementById("confirmar-senha");
 const checkCadastrar = document.getElementById("btn-cadastrar")
-
 
 const formCadastro = document.getElementById("form-cadastro");
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
-const unidade = document.querySelector('input[name="location"]')
-const senha = document.getElementById("senha");
+const inputSaoPaulo = document.getElementById("sao-paulo");
+const inputSantos = document.getElementById("santos");
+const inputHomeOffice = document.getElementById("home-office");
+
 
 
 formCadastro.addEventListener("submit", function(e) {    
     e.preventDefault()
 
-    console.log(nome.value, email.value, unidade.value, senha.value);
+    if (confirmarSenha.value != senha.value) {
+        swal("🍊", "as senhas não correspondem")
+        return;
+    } else if (senha.value == 0 || confirmarSenha == 0) {
+        swal("🍊", "não pode deixar os campos vazios")
+        return;
+    }
+    
+    let unidade;
+    if (inputSaoPaulo.checked) {
+        unidade = inputSaoPaulo.value;
+    } else if (inputSantos.checked) {
+        unidade = inputSantos.value;
+    } else if (inputHomeOffice.checked) {
+        unidade = inputHomeOffice.value;
+    }
+    console.log(nome.value, email.value, unidade, senha.value);
 
     const user = {
         name: nome.value, 
         email: email.value, 
-        location: unidade.value, 
+        location: unidade, 
         password: senha.value
     }
     console.log(user)
     console.log(typeof (user))
     fetch("http://127.0.0.1:3000/users", {
         method: 'POST',
-
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(user)
-    }).then(() =>{
-        window.location = '../pages/login.html'
-        swal("🍊 Little Juice!", "você vai receber um e-mail com um link de confirmação", "success");
-    })
+    }).then(response =>{
+        if(!response.ok){
+            response.text().then(text =>{
+                swal("🍊 Ops!", "ocorreu um erro. Tente novamente!"); 
+                formCadastro.reset();
+            })
+        }else{
+            // swal("🍊 Little Juice!", "você vai receber um e-mail com um link de confirmação", "success");
+            window.location = '../pages/login.html'
+        }
 
-    if (inputConfirmarSenha.value != inputSenha.value) {
-        swal("🍊", "as senhas não correspondem")
-    } else if (inputSenha.value == 0 || inputConfirmarSenha == 0) {
-        swal("🍊", "não pode deixar os campos vazios")
-    }
+    })
+    
+
+
 })
 
 // inputConfirmarCadastrar.oninput = checkSenha;
